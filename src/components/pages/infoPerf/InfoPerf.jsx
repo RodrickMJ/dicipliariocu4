92f404862fc3e6ebb5fe798031872c86416943da
RodrickMJ <231199@ids.upchiapas.edu.mx>
2024-04-22 02:25:58 -0600

    xd

--- src/components/pages/infoPerf/InfoPerf.jsx
+++ src/components/pages/infoPerf/InfoPerf.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import './infoPerf.css'
-import Images from '../../ui/images/Images'
-import Titles from '../../ui/titles/Titles'
-import Pharagraps from '../../ui/pharagraps/Pharagraps'
-// import { FaXTwitter } from "react-icons/fa6";
-// import { FaFacebookF } from "react-icons/fa6";
-// import { IoLogoInstagram } from "react-icons/io5";
-import { PerfilPru } from '../../../utils/PerfilPru'
+import React, { useState } from 'react';
+import './infoPerf.css';
+import Images from '../../ui/images/Images';
+import Titles from '../../ui/titles/Titles';
+import Pharagraps from '../../ui/pharagraps/Pharagraps';
 import { LuPhoneCall } from "react-icons/lu";
-import Buttons from '../../ui/buttons/Buttons'
-import Inputs from '../../ui/input/Inputs'
+import Buttons from '../../ui/buttons/Buttons';
+import Inputs from '../../ui/input/Inputs';
+import { PerfilPru } from '../../../utils/PerfilPru';
 
 function InfoPerf() {
-    const perfil = PerfilPru.find(perfil => perfil.id === 1); 
-  return (
-    <>
+    const perfil = PerfilPru.find(perfil => perfil.id === 1);
+    const [mostrarFormulario, setMostrarFormulario] = useState(false);
+    const [nombre, setNombre] = useState('');
+    const [email, setEmail] = useState('');
+    const [contacto, setContacto] = useState('');
+    const [direccion, setDireccion] = useState('');
+
+    const handleMostrarFormulario = () => {
+        setMostrarFormulario(true);
+    };
+
+    const handleCancelar = () => {
+        setMostrarFormulario(false);
+    };
+
+    const handleEnviar = (e) => {
+        e.preventDefault();
+        console.log("Enviando datos actualizados:", { nombre, email, contacto, direccion });
+        setMostrarFormulario(false);
+    };
+
+    return (
+        <>
         <div className='contPerf'>
             <div className='imgsPerf'>
                 <Images customClas="imgPortPer" image={perfil.imgPor} />
@@ -24,36 +41,43 @@ function InfoPerf() {
                 <Titles customClass="nameUserF" text={perfil.nombre} />
                 <div className='dircUseCont' >
                     <Titles customClass="directionUserf" text="Direccion" />
-                <Pharagraps customClass="" text={perfil.direccion} />
+                    <Pharagraps customClass="" text={perfil.direccion} />
                 </div>
             </div>
             <div className='contperfCR'>
-                <Titles customClass="titlecontF" text="Contacto"/>
+                <Titles customClass="titlecontF" text="Contacto" />
                 <ul>
-                    <li><LuPhoneCall size={20} /><Pharagraps customClass="contRdUser" text={perfil.contacto}/></li>
-                    {/* <li><a href="#"><FaXTwitter size={20} /> <Pharagraps customClass="contRdUser" text={perfil.contacto}/> </a></li>
-                    <li><a href="#"><FaFacebookF size={20} /> <Pharagraps customClass="contRdUser" text={perfil.contacto}/> </a></li>
-                    <li><a href="#"><IoLogoInstagram size={20} /> <Pharagraps customClass="contRdUser" text={perfil.contacto}/> </a></li> */}
+                    <li><LuPhoneCall size={20} /><Pharagraps customClass="contRdUser" text={perfil.contacto} /></li>
                 </ul>
             </div>
             <div>
-                <Buttons content="" customClass="" Handler={handleMostrarFormulario} /> 
-
-                <form>
-                        <Titles text="" customClass="" />
+                <Buttons content="Actualizar perfil" customClass=""/>
+                <form >
                         <div>
-                            <Titles />
-                            <Inputs type="text" placeholder="Nombre" customClass="input-field" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+                            <Titles text="Nombre" customClass="" />
+                            <Inputs type="text" placeholder="Nombre" />
                         </div>
-                </form>
+                        <div>
+                            <Titles text="Email" customClass="" />
+                            <Inputs type="text" placeholder="Email"  />
+                        </div>
+                        <div>
+                            <Titles text="Contacto" customClass="" />
+                            <Inputs type="text" placeholder="Contacto" />
+                        </div>
+                        <div>
+                            <Titles text="Direccion" customClass="" />
+                            <Inputs type="text" placeholder="Direccion"  />
+                        </div>
+                        <div className='mozcontb'>
+                            <Buttons content="Enviar" customClass="submit-buttonA" type="submit" />
+                            <Buttons content="Cancelar" customClass="submit-buttonC"  type="submit" />
+                        </div>
+                    </form>
             </div>
         </div>
-    </>
-  )
+        </>
+    );
 }
 
-export default InfoPerf
-
-// 1095342397650
-
-// strange-team-420010
+export default InfoPerf;
